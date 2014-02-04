6b12f2a285724f466b56669ce4e4c166b26b0f63
JayChetty <jaypaulchetty@gmail.com>
2014-02-04 20:19:43 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Calendar::Application.config.secret_key_base = '4fb4f877a6896d686f660a49dbbaa29332635ca7e282c305dffc6b55b36f90d253436a666df2904300e97f8fbd96e77778aa5e4e24bd5e76f1cb06152d44c30c'
