e0e4a875c8363016aedc30c996b19f8fbfc1ba1c
JayChetty <jaypaulchetty@gmail.com>
2014-04-07 20:50:34 +0100

    fix test

--- spec/models/earth_cycles_spec.rb
+++ spec/models/earth_cycles_spec.rb
@@ -9,15 +9,15 @@ describe EarthCycles do
   end
 
   it "should show the days since new moon" do 
-    first_day_of_month = Time.new(2012,11,29,12,00)
+    first_day_of_month = Time.new(2014,03,31,12,00)
     earth = EarthCycles.new(first_day_of_month)
     earth.day_of_month.should == 1  
   end
 
-  it "should show the first new moonh of year" do
-    day_of_year = Time.new(2012,12,25,12,00)
+  it "should show the first new moonth of year" do
+    day_of_year = Time.new(2014,6,6,12,00)
     earth = EarthCycles.new(day_of_year)
-    earth.first_new_moon_of_year.to_i.should == Time.new(2012,12,28,03,29,02).to_i     
+    earth.first_new_moon_of_year.should be_within(1.day).of(Time.new(2014,01,01,04,15,02))
   end
 
   it "should show the month of year" do
